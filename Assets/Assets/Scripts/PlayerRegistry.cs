fc97cfd39c344ab169e82fc0acb2ffd2dee1d42b
bazaam <bazaam1@gmail.com>
2017-02-21 14:51:18 -0800

    variable name fix and planet prefab collider fix

--- Assets/Assets/Scripts/PlayerRegistry.cs
+++ Assets/Assets/Scripts/PlayerRegistry.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerRegistry : MonoBehaviour {
-    Dictionary<int, Color32> playerColors;
+    Dictionary<int, Color32> PlayerColors;
 
 	void Awake()
     {
-        playerColors = new Dictionary<int, Color32>();
-        playerColors.Add(0, new Color32(97, 97, 97, 255));
-        playerColors.Add(1, new Color32(199, 9, 13, 255));
-        playerColors.Add(2, new Color32(27, 28, 244, 255));
+        PlayerColors = new Dictionary<int, Color32>();
+        PlayerColors.Add(0, new Color32(97, 97, 97, 255));
+        PlayerColors.Add(1, new Color32(199, 9, 13, 255));
+        PlayerColors.Add(2, new Color32(27, 28, 244, 255));
     }
 
     public Color32 GetPlayerColor(int player)
     {
-        return playerColors[player];
+        return PlayerColors[player];
     }
 	
 }
