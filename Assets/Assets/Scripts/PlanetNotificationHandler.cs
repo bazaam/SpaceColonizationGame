4c9122803eb98a63c04bf3c0f1a5fabd33759fd9
Smoose3 <nickdeboisblanc@gmail.com>
2017-02-24 17:41:46 -0800

    notification script

---
+++ Assets/Assets/Scripts/PlanetNotificationHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetParticles : MonoBehaviour
+{
+    private var planetParticles : ParticleSystem;
+
+    function Start ()
+    {
+        planetParticles = GetComponent(ParticleSystem);
+        planetParticles.Play();
+    }
+}
