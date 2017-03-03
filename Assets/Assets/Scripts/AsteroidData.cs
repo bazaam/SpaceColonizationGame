499e9a05d3618c4e506ac52b8506db7caf98b06a
Smoose3 <nickdeboisblanc@gmail.com>
2017-03-03 15:04:22 -0800

    Created Asteroid Data & Unit Handler Scripts, and made an Asteroid prefab

---
+++ Assets/Assets/Scripts/AsteroidData.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidData : MonoBehaviour
+{
+    public GameObject planetOwnershipOverlay;
+    public string planetType;
+    public int basePopulationCap;
+
+    private int populationCap;
+    private int numberOfUnits = 0;
+    private GameObject owner;
+
+    PlanetTypeRegistry planetTypeRegistry;
+
+        void Start()
+    {
+
+    }
+	
+	void Update ()
+    {
+		
+	}
+    public void ReleaseUnits(int quantity)
+    {
+        numberOfUnits -= quantity;
+        //Debug.Log(this.name + "lost" + quantity + "units, and now has" + numberOfUnits + "units,");
+    }
+
+    public void AddUnits(int quantity, GameObject unitsOwner)
+    {
+        if (unitsOwner == owner)
+            numberOfUnits += quantity;
+        else
+        {
+            if (quantity > numberOfUnits)
+            {
+                numberOfUnits = (quantity - numberOfUnits);
+                UpdateOwner(unitsOwner);
+            }
+            else
+                numberOfUnits -= quantity;
+        }
+    }
+
+    public int GetNumberOfUnits()
+    {
+        return numberOfUnits;
+    }
+
+    public GameObject GetOwner()
+    {
+        return owner;
+    }
+
+    public void UpdateOwner(GameObject newOwner)
+    {
+        if (newOwner != owner)
+        {
+            PlayerData ownerData;
+
+            if (owner != null)
+            {
+                ownerData = owner.GetComponent<PlayerData>();
+                ownerData.UnregisterPlanet(gameObject);
+                ownerData.UpdatePlayerStats();
+                ownerData.UpdatePlanetStats();
+            }
+            owner = newOwner;
+            ownerData = owner.GetComponent<PlayerData>();
+            ownerData.RegisterPlanet(gameObject);
+            ownerData.UpdatePlayerStats();
+            ownerData.UpdatePlanetStats();
+        }
+        planetOwnershipOverlay.GetComponent<SpriteRenderer>().color = PlayerRegistry.instance.GetPlayerColor(newOwner.GetComponent<PlayerData>().PlayerID);
+    }
+
+    public int GetPopulationCap()
+    {
+        return basePopulationCap;
+    }
+
+    public void UpdatePlanetStats()
+    {
+        populationCap = (int)Mathf.Round(basePopulationCap * (1 + owner.GetComponent<PlayerData>().PopulationCapModifier));
+    }
+}
---
+++ Assets/Assets/Scripts/AsteroidUnitHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidUnitHandler : MonoBehaviour
+{
+    public GameObject travellingUnitsPrefab;
+    AsteroidData data;
+
+    private void Start()
+    {
+        data = this.GetComponent<AsteroidData>();
+    }
+
+    public void SendUnits(GameObject targetPlanet, GameObject planetOwner)
+    {
+        int unitsToRelease = (data.GetNumberOfUnits() / 2);
+        data.ReleaseUnits(unitsToRelease);
+        TravellingUnit units = Instantiate(travellingUnitsPrefab, this.gameObject.transform.position, Quaternion.identity).GetComponent<TravellingUnit>();
+        units.Initialize(unitsToRelease, data.GetOwner(), targetPlanet, 10.0f * (1 + planetOwner.GetComponent<PlayerData>().UnitSpeedModifier));
+    }
+    
+    private void UpdatePopulation()
+    {
+        if (data.GetNumberOfUnits() > data.GetPopulationCap())
+            data.ReleaseUnits(1);
+    }
+
+    private IEnumerator DecayPopulation()
+    {
+        for (;;)
+        {
+            if (data.GetNumberOfUnits() > data.GetPopulationCap())
+                UpdatePopulation();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
