2c15bddd24040d0e0fd9cf652443299a4714e57a
bazaam <bazaam1@gmail.com>
2017-03-03 09:41:23 -0800

    new planet prefab "Jupiter"

--- Assets/Assets/Scripts/PlanetTypeRegistry.cs
+++ Assets/Assets/Scripts/PlanetTypeRegistry.cs
@@ -8,7 +8,7 @@ public class PlanetTypeRegistry : MonoBehaviour {
 	void Awake()
     {
         PlanetTypeColors = new Dictionary<string, Color32>();
-        PlanetTypeColors.Add("Normal", new Color32(97, 97, 97, 255));
+        PlanetTypeColors.Add("Normal", new Color32(255, 255, 255, 255));
         PlanetTypeColors.Add("Iron", new Color32(223, 135, 62, 147));
         PlanetTypeColors.Add("Carbon", new Color32(73, 191, 108, 255));
         PlanetTypeColors.Add("Farm", new Color32(192, 137, 225, 255));
